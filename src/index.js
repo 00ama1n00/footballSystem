a782013ba4cb4b4f2e14a876cc89fe2b76b51f16
Kaleb Yohannese Tewelde <125556510+KalebYohannese@users.noreply.github.com>
2025-09-06 14:26:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// testt 56
+
 // REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
