a5cbc3be4d0f187912362686b86e0ffae6f40d13
AMANAN AMANUEL <amananamanuel@outlook.com>
2025-09-12 10:39:31 +0300

    Add titles to content summaries in App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 const content = [
   {
+    title: "summary 1",
     summary: "React a library",
     details:
       "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    title: "summary 2",
     summary: "State management",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    title: "summary 3",
     summary: "We can think of props",
     details:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
